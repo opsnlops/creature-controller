37323b0907f9cb39674e0a4680a3bfe4ff503ff2
April White <april@opsnlops.io>
2023-12-07 14:35:47 -0800

    Phase one of moving things around

---
+++ firmware/src/usb/usb.h
@@ -0,0 +1,8 @@
+//
+// Created by April White on 12/7/23.
+//
+
+#ifndef CREATURE_CONTROLLER_USB_H
+#define CREATURE_CONTROLLER_USB_H
+
+#endif //CREATURE_CONTROLLER_USB_H
---
+++ firmware/src/usb/usb_descriptors.cpp
@@ -0,0 +1,3 @@
+//
+// Created by April White on 12/7/23.
+//
