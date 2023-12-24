267df09075faee559c99800bb0e8cfab76136b9f
April White <april@opsnlops.io>
2023-12-23 23:20:33 -0800

    My name is GCC and I like to complain

--- firmware/src/freertos_hooks.cpp
+++ firmware/src/freertos_hooks.cpp
@@ -16,6 +16,19 @@ extern "C" void vApplicationMallocFailedHook( void )
 }
 
 
+
+void vApplicationStackOverflowHook(TaskHandle_t xTask, const char* pcTaskName) {
+
+    // Halt the system on a stack overflow.
+    taskDISABLE_INTERRUPTS();
+
+    // Log the overflow. Replace this with your logging method.
+    printf("Stack overflow in task: %s\n", pcTaskName);
+
+    // Assert to halt the system
+    configASSERT(true);
+}
+
 void vApplicationIdleHook( void )
 {
 
