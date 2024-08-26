9a8574dfd32da07e1f253f882ea15d01a7381ae7
April White <april@opsnlops.io>
2024-08-25 17:31:58 -0700

    Pre MFM2024

--- firmware/controller-config.h
+++ firmware/controller-config.h
@@ -122,7 +122,7 @@ typedef uint64_t u64;
 #define LOGGING_LEVEL               LOG_LEVEL_DEBUG
 #define LOGGING_QUEUE_LENGTH        100
 #define LOGGING_MESSAGE_MAX_LENGTH  256
-#define LOGGING_LOG_VIA_PRINTF      1           // Add a printf() in the logger. Useful when a debugger is attached
+#define LOGGING_LOG_VIA_PRINTF      0           // Add a printf() in the logger. Useful when a debugger is attached
 
 /*
  * Message Processor Config
