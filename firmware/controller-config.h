79a174d3533fc9cfef44a5366e3ceee2e502a49a
April White <april@opsnlops.io>
2024-10-05 13:53:54 -0700

    Clean up `CMakeLists.txt`

--- firmware/controller-config.h
+++ firmware/controller-config.h
@@ -49,8 +49,8 @@ typedef uint64_t u64;
 #define STATUS_LIGHTS_TIME_MS               20
 #define STATUS_LIGHTS_PIO                   pio1
 
-//#define STATUS_LIGHTS_LOGIC_BOARD_PIN       17
-#define STATUS_LIGHTS_LOGIC_BOARD_PIN       22
+#define STATUS_LIGHTS_LOGIC_BOARD_PIN       17
+//#define STATUS_LIGHTS_LOGIC_BOARD_PIN       22
 #define STATUS_LIGHTS_LOGIC_BOARD_IS_RGBW   false
 
 // Max brightness of the lights on the servo modules. Max is 255.
@@ -123,7 +123,7 @@ typedef uint64_t u64;
 #define LOGGING_LEVEL               LOG_LEVEL_DEBUG
 #define LOGGING_QUEUE_LENGTH        100
 #define LOGGING_MESSAGE_MAX_LENGTH  256
-#define LOGGING_LOG_VIA_PRINTF      1           // Add a printf() in the logger. Useful when a debugger is attached
+#define LOGGING_LOG_VIA_PRINTF      0           // Add a printf() in the logger. Useful when a debugger is attached
 
 /*
  * Message Processor Config
